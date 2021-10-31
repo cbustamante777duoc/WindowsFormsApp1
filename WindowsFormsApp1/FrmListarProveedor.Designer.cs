f79c18a473e03b2dd73dddfe9d079c139438f8cb
cristian <cra.bustamante@alumnos.duoc.cl>
2021-10-30 23:37:22 -0300

    filtrar por rut

--- WindowsFormsApp1/FrmListarProveedor.Designer.cs
+++ WindowsFormsApp1/FrmListarProveedor.Designer.cs
@@ -30,6 +30,9 @@ namespace WindowsFormsApp1
         private void InitializeComponent()
         {
             this.dgvProveedor = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtRut = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvProveedor)).BeginInit();
             this.SuspendLayout();
             // 
@@ -38,27 +41,60 @@ namespace WindowsFormsApp1
             this.dgvProveedor.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvProveedor.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
             this.dgvProveedor.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvProveedor.Location = new System.Drawing.Point(72, 63);
+            this.dgvProveedor.Location = new System.Drawing.Point(74, 103);
             this.dgvProveedor.Name = "dgvProveedor";
             this.dgvProveedor.Size = new System.Drawing.Size(543, 253);
             this.dgvProveedor.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Buscar por RUT";
+            // 
+            // txtRut
+            // 
+            this.txtRut.Location = new System.Drawing.Point(183, 67);
+            this.txtRut.Name = "txtRut";
+            this.txtRut.Size = new System.Drawing.Size(263, 20);
+            this.txtRut.TabIndex = 2;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(479, 56);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(99, 31);
+            this.btnBuscar.TabIndex = 3;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
             // 
             // FrmListarProveedor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtRut);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.dgvProveedor);
             this.Name = "FrmListarProveedor";
             this.Text = "FrmListarProveedor";
             this.Load += new System.EventHandler(this.FrmListarProveedor_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvProveedor)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dgvProveedor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtRut;
+        private System.Windows.Forms.Button btnBuscar;
     }
 }
