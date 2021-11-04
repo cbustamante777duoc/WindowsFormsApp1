4618fd383940080a2ae4210654d0ea36aa2e24b8
cristian <cra.bustamante@alumnos.duoc.cl>
2021-11-03 21:20:59 -0300

    insertar proveedor en frmListarProveedor

--- WindowsFormsApp1/FrmListarProveedor.Designer.cs
+++ WindowsFormsApp1/FrmListarProveedor.Designer.cs
@@ -31,9 +31,27 @@ namespace WindowsFormsApp1
         {
             this.dgvProveedor = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
-            this.txtRut = new System.Windows.Forms.TextBox();
+            this.txtBuscarRut = new System.Windows.Forms.TextBox();
             this.btnBuscar = new System.Windows.Forms.Button();
             this.btnLimpiar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.txtRut = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.txtComuna = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnInsertar = new System.Windows.Forms.Button();
+            this.btnCargar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvProveedor)).BeginInit();
             this.SuspendLayout();
             // 
@@ -42,7 +60,7 @@ namespace WindowsFormsApp1
             this.dgvProveedor.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvProveedor.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
             this.dgvProveedor.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvProveedor.Location = new System.Drawing.Point(74, 103);
+            this.dgvProveedor.Location = new System.Drawing.Point(194, 283);
             this.dgvProveedor.Name = "dgvProveedor";
             this.dgvProveedor.Size = new System.Drawing.Size(543, 253);
             this.dgvProveedor.TabIndex = 0;
@@ -50,22 +68,22 @@ namespace WindowsFormsApp1
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(71, 67);
+            this.label1.Location = new System.Drawing.Point(191, 227);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(84, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Buscar por RUT";
             // 
-            // txtRut
+            // txtBuscarRut
             // 
-            this.txtRut.Location = new System.Drawing.Point(183, 67);
-            this.txtRut.Name = "txtRut";
-            this.txtRut.Size = new System.Drawing.Size(263, 20);
-            this.txtRut.TabIndex = 2;
+            this.txtBuscarRut.Location = new System.Drawing.Point(303, 227);
+            this.txtBuscarRut.Name = "txtBuscarRut";
+            this.txtBuscarRut.Size = new System.Drawing.Size(263, 20);
+            this.txtBuscarRut.TabIndex = 2;
             // 
             // btnBuscar
             // 
-            this.btnBuscar.Location = new System.Drawing.Point(452, 58);
+            this.btnBuscar.Location = new System.Drawing.Point(572, 218);
             this.btnBuscar.Name = "btnBuscar";
             this.btnBuscar.Size = new System.Drawing.Size(99, 31);
             this.btnBuscar.TabIndex = 3;
@@ -75,7 +93,7 @@ namespace WindowsFormsApp1
             // 
             // btnLimpiar
             // 
-            this.btnLimpiar.Location = new System.Drawing.Point(567, 58);
+            this.btnLimpiar.Location = new System.Drawing.Point(687, 218);
             this.btnLimpiar.Name = "btnLimpiar";
             this.btnLimpiar.Size = new System.Drawing.Size(92, 32);
             this.btnLimpiar.TabIndex = 4;
@@ -83,14 +101,182 @@ namespace WindowsFormsApp1
             this.btnLimpiar.UseVisualStyleBackColor = true;
             this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(71, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "id_proveedor";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(168, 30);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(127, 20);
+            this.txtId.TabIndex = 11;
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(168, 162);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(127, 20);
+            this.txtTelefono.TabIndex = 28;
+            // 
+            // txtRut
+            // 
+            this.txtRut.Location = new System.Drawing.Point(456, 30);
+            this.txtRut.Name = "txtRut";
+            this.txtRut.Size = new System.Drawing.Size(127, 20);
+            this.txtRut.TabIndex = 27;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(169, 64);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(127, 20);
+            this.txtNombre.TabIndex = 26;
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(456, 113);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(127, 20);
+            this.txtDireccion.TabIndex = 25;
+            // 
+            // txtComuna
+            // 
+            this.txtComuna.Location = new System.Drawing.Point(168, 109);
+            this.txtComuna.Name = "txtComuna";
+            this.txtComuna.Size = new System.Drawing.Size(127, 20);
+            this.txtComuna.TabIndex = 24;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(456, 67);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(127, 20);
+            this.txtApellido.TabIndex = 23;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(387, 30);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(24, 13);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "Rut";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(71, 64);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 13);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "Nombre";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(66, 169);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Telefono";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(387, 116);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Direccion";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(69, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Comuna";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(387, 67);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(44, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Apellido";
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(754, 109);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(82, 44);
+            this.btnEliminar.TabIndex = 32;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(749, 30);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(87, 44);
+            this.btnActualizar.TabIndex = 31;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // btnInsertar
+            // 
+            this.btnInsertar.Location = new System.Drawing.Point(628, 30);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(91, 44);
+            this.btnInsertar.TabIndex = 30;
+            this.btnInsertar.Text = "Insertar";
+            this.btnInsertar.UseVisualStyleBackColor = true;
+            this.btnInsertar.Click += new System.EventHandler(this.btnInsertar_Click);
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Location = new System.Drawing.Point(628, 109);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(91, 44);
+            this.btnCargar.TabIndex = 29;
+            this.btnCargar.Text = "cargar";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            // 
             // FrmListarProveedor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(867, 450);
+            this.ClientSize = new System.Drawing.Size(966, 546);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnInsertar);
+            this.Controls.Add(this.btnCargar);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.txtRut);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtDireccion);
+            this.Controls.Add(this.txtComuna);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.btnLimpiar);
             this.Controls.Add(this.btnBuscar);
-            this.Controls.Add(this.txtRut);
+            this.Controls.Add(this.txtBuscarRut);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dgvProveedor);
             this.Name = "FrmListarProveedor";
@@ -106,8 +292,26 @@ namespace WindowsFormsApp1
 
         private System.Windows.Forms.DataGridView dgvProveedor;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox txtRut;
+        private System.Windows.Forms.TextBox txtBuscarRut;
         private System.Windows.Forms.Button btnBuscar;
         private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.TextBox txtRut;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.TextBox txtComuna;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnInsertar;
+        private System.Windows.Forms.Button btnCargar;
     }
 }
--- WindowsFormsApp1/FrmListarProveedor.cs
+++ WindowsFormsApp1/FrmListarProveedor.cs
@@ -14,6 +14,7 @@ namespace WindowsFormsApp1
 {
     public partial class FrmListarProveedor : Form
     {
+        OracleConnection ora = new OracleConnection("DATA SOURCE =XE; PASSWORD=abcd1234; USER ID =SIGLOXXI");
         public FrmListarProveedor()
         {
             InitializeComponent();
@@ -30,7 +31,7 @@ namespace WindowsFormsApp1
             OracleCommand cmd = new OracleCommand("BuscarProveedor", cn);
             cmd.CommandType = CommandType.StoredProcedure;
             cmd.Parameters.Add("cursorMemoria", OracleType.Cursor).Direction = ParameterDirection.Output;
-            cmd.Parameters.Add("rutB", OracleType.VarChar).Value = txtRut.Text;
+            cmd.Parameters.Add("rutB", OracleType.VarChar).Value = txtBuscarRut.Text;
             OracleDataAdapter adapter = new OracleDataAdapter(cmd);
             DataTable table = new DataTable();
             adapter.Fill(table);
@@ -39,8 +40,61 @@ namespace WindowsFormsApp1
 
         private void btnLimpiar_Click(object sender, EventArgs e)
         {
-            txtRut.Text = "";
+            txtBuscarRut.Text = "";
             SQL.ListarProcedureSql("ListarProveedor", "cursorMemoria", dgvProveedor);
+        }
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ora.Open();
+                OracleCommand commando = new OracleCommand("insertarProveedor", ora);
+                commando.CommandType = System.Data.CommandType.StoredProcedure;
+                commando.Parameters.Add("id_proveedor", OracleType.VarChar).Value = txtId.Text;
+                commando.Parameters.Add("apellido", OracleType.VarChar).Value = txtApellido.Text;
+                commando.Parameters.Add("comuna", OracleType.VarChar).Value = txtComuna.Text;
+                commando.Parameters.Add("direccion", OracleType.VarChar).Value = txtDireccion.Text;
+                commando.Parameters.Add("nombre", OracleType.VarChar).Value = txtNombre.Text;
+                commando.Parameters.Add("rut", OracleType.VarChar).Value = txtRut.Text;
+                commando.Parameters.Add("telefono", OracleType.VarChar).Value = txtTelefono.Text;
+                commando.ExecuteNonQuery();
+                MessageBox.Show("se inserto correctamente el proveedor en la base de datos con id =" + txtId.Text);
+
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("algo fallo");
+            }
+
+            ora.Close();
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ora.Open();
+                OracleCommand commando = new OracleCommand("actualizarProveedor", ora);
+                commando.CommandType = System.Data.CommandType.StoredProcedure;
+                commando.Parameters.Add("id_porveedor", OracleType.VarChar).Value = txtId.Text;
+                commando.Parameters.Add("apellido", OracleType.VarChar).Value = txtApellido.Text;
+                commando.Parameters.Add("comuna", OracleType.VarChar).Value = txtComuna.Text;
+                commando.Parameters.Add("direccion", OracleType.VarChar).Value = txtDireccion.Text;
+                commando.Parameters.Add("nombre", OracleType.VarChar).Value = txtNombre.Text;
+                commando.Parameters.Add("rut", OracleType.VarChar).Value = txtRut.Text;
+                commando.Parameters.Add("telefono", OracleType.VarChar).Value = txtTelefono.Text;
+                commando.ExecuteNonQuery();
+                MessageBox.Show("se actualizo correctamente el proveedor en la base de datos con id = " + txtId.Text);
+
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("se actualizo correctamente el proveedor en la base de datos");
+            }
+            ora.Close();
         }
     }
 }
